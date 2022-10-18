532a8e7af8c961fb278a81f5f8239a6c65707b02
hxexxn <102952683+hxexxn@users.noreply.github.com>
2022-10-18 15:46:22 +0900

    first commit

---
+++ src/main/java/com/portfolio/www/KhePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhePortfolioApplication.class, args);
+	}
+
+}
