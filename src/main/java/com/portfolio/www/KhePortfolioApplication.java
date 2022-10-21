4d2568e2b3095ecd154fed143731f35cc54cf5ed
hxexxn <102952683+hxexxn@users.noreply.github.com>
2022-10-21 22:12:29 +0900

    index 페이지
    수정 중

--- src/main/java/com/portfolio/www/KhePortfolioApplication.java
+++ src/main/java/com/portfolio/www/KhePortfolioApplication.java
@@ -2,8 +2,9 @@ package com.portfolio.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class KhePortfolioApplication {
 
 	public static void main(String[] args) {
